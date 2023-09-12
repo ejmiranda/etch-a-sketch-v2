f545527e15ab2b227a2894e6192e9fdbe6d63561
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-12 00:08:08 -0500

    Get window height and width, not incl. toolbars, et.

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,6 @@ const grid = document.querySelector('.grid');
 const runBtn = document.getElementById('run-btn');
 
 runBtn.addEventListener('click', (event) => {
-  console.log(event.target);
+  console.log(`Height: ${window.innerHeight}`);
+  console.log(`Width: ${window.innerWidth}`);
 });
