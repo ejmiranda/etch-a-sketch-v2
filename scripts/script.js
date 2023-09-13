621c920201d6bc5bf1cdc7a80788c361884f00dc
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-13 13:48:14 +0000

    Add reset button to unload grid

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const heightP = document.querySelector('.height');
 const widthP = document.querySelector('.width');
 const cellSizeP = document.querySelector('.cell-size');
 const cellCountP = document.querySelector('.cell-count');
-const drawBtn = document.getElementById('draw');
+const resetBtn = document.getElementById('reset');
 
 const grid = document.querySelector('.grid');
 const cell = document.querySelector('.cell');
@@ -11,13 +11,14 @@ const cell = document.querySelector('.cell');
 let gHeight;
 let gWidth;
 let cellSize = 30;
+let cellQty;
 
 window.addEventListener('load', loadGrid);
 window.addEventListener('resize', updateMeasures);
-drawBtn.addEventListener('click', unloadGrid);
+resetBtn.addEventListener('click', unloadGrid);
 
 function loadGrid() {
-  updateMeasures();
+  getMeasures();
   let cellQty = (gHeight/cellSize) * (gWidth/cellSize);
   for (let i = 0; i < cellQty; i++) {
     let cell = document.createElement('div');
@@ -25,16 +26,21 @@ function loadGrid() {
     cell.style.height = `${cellSize}px`;
     cell.style.width = `${cellSize}px`;
     grid.appendChild(cell);
-    cellCountP.textContent = `Cell Count = ${getCellCount()}`;
   }
+  updateMeasures();
+}
+
+function getMeasures() {
+  gHeight = window.innerHeight - measuresDiv.offsetHeight;
+  gWidth = window.innerWidth;
 }
 
 function updateMeasures () {
-  gHeight = window.innerHeight - measuresDiv.offsetHeight;
-  gWidth = window.innerWidth;
+  getMeasures();
   heightP.textContent = `Grid Height = ${gHeight}px`;
   widthP.textContent = `Grid Width = ${gWidth}px`;
   cellSizeP.textContent = `Cell Size = ${cellSize}px`;
+  cellCountP.textContent = `Cell Count = ${getCellCount()}`
 }
 
 function getCellCount() {
@@ -45,4 +51,5 @@ function unloadGrid() {
   while (grid.hasChildNodes()) {
     grid.removeChild(grid.firstChild);
   }  
+  updateMeasures();
 }
