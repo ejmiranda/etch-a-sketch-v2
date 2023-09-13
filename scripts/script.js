20b2756d400937d85d4a3a6c06916a1b675d6e49
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-13 01:18:15 -0500

    Add function to fill the screen with cells

--- scripts/script.js
+++ scripts/script.js
@@ -10,31 +10,32 @@ const cell = document.querySelector('.cell');
 
 let gHeight;
 let gWidth;
-let cellSize;
+let cellSize = 30;
 
 window.addEventListener('load', updateMeasures);
 window.addEventListener('resize', updateMeasures);
+drawBtn.addEventListener('click', fillGrid);
 
 function updateMeasures () {
   gHeight = window.innerHeight - measuresDiv.offsetHeight;
   gWidth = window.innerWidth;
-  cellSize = gWidth/50;
   heightP.textContent = `Grid Height = ${gHeight}px`;
   widthP.textContent = `Grid Width = ${gWidth}px`;
   cellSizeP.textContent = `Cell Size = ${cellSize}px`;
 }
 
-function getCellCount() {
-  return grid.children.length;
-}
-
-drawBtn.addEventListener('click', () => {
-  for (let i=0; i<2; i++) {
+function fillGrid() {
+  let cellQty = (gHeight/cellSize) * (gWidth/cellSize);
+  for (let i = 0; i < cellQty; i++) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.style.height = `${30}px`;
-    // cell.style.width = `${30}px`;
+    cell.style.height = `${cellSize}px`;
+    cell.style.width = `${cellSize}px`;
     grid.appendChild(cell);
     cellCountP.textContent = `Cell Count = ${getCellCount()}`;
   }
-});
+}
+
+function getCellCount() {
+  return grid.children.length;
+}
